445fe4df796474550bc31be1d4b028e75a983a76
Georgy Spiridonov <gosha583@gmail.com>
2022-03-17 22:24:50 +0300

    Update code

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5209259345:AAFrDyyZd_Y6sXj63x4KD6BCI3t4yy0PoqY"
 APP_URL = "https://kursovoyproectbot.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://tnebcefacmuqvk:f47eba0e9a6c9684ed48506fde40198da601264b4274edaba561aa680e455973@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d4vdir2cr5lf3d"
