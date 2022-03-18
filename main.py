47bc1903ea40f63f59d28d4f983e021c56523b17
Georgy Spiridonov <gosha583@gmail.com>
2022-03-18 03:23:55 +0300

    Update

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
 
 
+def update_message_count(user_id):
+    db_object.execute(f'UPDATE users SET message = messages + 1 WHERE id = {user_id}')
+    db_connection.commit()
+
+
 @bot.message_handler(commands=["start"])
 def start(message):
-    id = message.from_user.id
+    user_id = message.from_user.id
     username = message.from_user.username
     bot.reply_to(message, f'Привет, {username}!')
 
@@ -26,6 +31,13 @@ def start(message):
     if not result:
         db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (id, username, 0))
         db_connection.commit()
+    update_message_count(user_id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_users():
+    user_id = message_from_user.id
+    update_message_count(user_id)
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])  # Перенаправление информации с сервера "HIROKU" в бота
@@ -40,5 +52,3 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=(os.environ.get("PORT", 5000)))
-
-
