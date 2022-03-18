18658ea4a3dfd23444783d83ff6b4ddfdb0b58d1
Georgy Spiridonov <gosha583@gmail.com>
2022-03-18 03:10:25 +0300

    123

--- main.py
+++ main.py
@@ -28,24 +28,6 @@ def start(message):
         db_connection.commit()
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_message(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, 'Как тебя зовут?')
-        bot.register_next_step_handler(message, get_name)
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши Привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-    db_object.execute(f"SELECT id FROM info_for_users WHERE id = {id}")
-    result = db_object.fetchone()  # Возвращает в кач-ве рез-та одну строчку с рез-том запроса
-
-    if not result:
-        db_object.execute("INSERT INTO info_for_users(id, Name) VALUES (%s, %s)", (id, Name))
-        db_connection.commit()
-
-
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])  # Перенаправление информации с сервера "HIROKU" в бота
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
@@ -54,9 +36,9 @@ def redirect_message():
     return "!", 200
 
 
-bot.polling(none_stop=True, interval=0)
-
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=(os.environ.get("PORT", 5000)))
+
+
